0455bdc003498101e9cc0ce291a043e12b8e49c0
Ilham Dedi <muhammadilhamdedi@gmail.com>
2018-09-18 01:43:16 +0800

    initial

---
+++ djangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/milhamdedi/Documents/learndev/l_django/ntusu-blog/djangoenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
